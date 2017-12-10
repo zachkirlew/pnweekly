7c633c7fba309e490bbf4b6799a17e203ef2210d
zachkirlew <zach_kirlew@hotmail.co.uk>
2017-12-10 21:23:26 +0000

    Some commit message

--- wsgi/pn_weekly/pn_weekly/settings.py
+++ wsgi/pn_weekly/pn_weekly/settings.py
@@ -136,6 +136,4 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-
-
+STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
