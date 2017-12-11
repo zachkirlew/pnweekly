1ac9e3c4654c05da7af4f7688671e330eb8b92c6
zachkirlew <zach_kirlew@hotmail.co.uk>
2017-12-11 00:16:15 +0000

    Some commit message

--- .openshift/cron/daily/articles_cron.sh
+++ .openshift/cron/daily/articles_cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /app-deployments/current/repo/wsgi/pnweekly
-/usr/bin/python manage.py getarticles
+cd ~/app-deployments/current/repo/wsgi/pn_weekly
+python manage.py getarticles
--- .openshift/cron/minutely/articles_cron.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ~/app-deployments/current/repo/wsgi/pn_weekly
-/usr/bin/python manage.py getarticles
