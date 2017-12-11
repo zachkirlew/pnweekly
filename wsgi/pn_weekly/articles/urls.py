089a3c0d58600f917066e738e7ceefdc6191a664
zachkirlew <zach_kirlew@hotmail.co.uk>
2017-12-11 19:09:50 +0000

    Some commit message

--- wsgi/pn_weekly/articles/urls.py
+++ wsgi/pn_weekly/articles/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     url(r'^article_detail/(?P<article_id>\d+)/$', views.article_detail, name='article_detail'),
     # POST add like to article
     url(r'^like_article/$', views.like_article, name='like_article'),
-    # GET all article comments
-    url(r'^get_comments/(?P<article_id>\d+)/$', views.get_comments, name='get_comments'),
     # POST new article comment
     url(r'^post_comment/$', views.post_comment, name='post_comment'),
     # DELETE article comment
