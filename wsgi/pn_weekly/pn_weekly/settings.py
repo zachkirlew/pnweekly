6d1332df11b939cb4aa1646a0fc567e4efc7f3d2
zachkirlew <zach_kirlew@hotmail.co.uk>
2017-12-13 13:55:04 +0000

    Debugging

--- wsgi/pn_weekly/pn_weekly/settings.py
+++ wsgi/pn_weekly/pn_weekly/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = ['pnweekly-group8.apps.devcloud.eecs.qmul.ac.uk', '127.0.0.1', '
 SECRET_KEY = 'lv0*cph(@)w&nv=9d1a4^_=d+!&+5bi1bdu)^8bcx6-+!z=4r&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Email config
 EMAIL_HOST = 'smtp.sendgrid.net'
