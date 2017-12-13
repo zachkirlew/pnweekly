ff80ccca18f311b8c3b539d953f1ca7d7365f4b3
zachkirlew <zach_kirlew@hotmail.co.uk>
2017-12-13 14:02:32 +0000

    Requirements change

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
         'Django==1.11.5',
         'django-cookie-law==1.0.13',
         'djangorestframework==3.7.3',
-        'requests==2.18.4'
+        'requests==2.18.4',
+        'Pillow==4.3.0'
     ],
     dependency_links=[
         'https://pypi.python.org/simple/django/'
